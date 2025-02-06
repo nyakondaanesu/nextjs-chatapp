b0a4c4fd03644147c0e012700d507c21baea86c7
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-06 14:00:00 +0200

    proper match making

--- server.js
+++ server.js
@@ -10,31 +10,37 @@ const handle = app.getRequestHandler();
 
 const startServer = async () => {
   try {
-    class room {
+    class Room {
       constructor(RoomId) {
         this.id = RoomId;
         this.users = [];
       }
-      addItem(item) {
-        if (typeof item === "string" && this.users.length < 2) {
-          this.users.push(item);
+
+      addUser(userId) {
+        if (this.users.length < 2) {
+          this.users.push(userId);
+          return true;
         }
+        return false;
       }
 
-      getItems() {
-        return this.users.length;
+      removeUser(userId) {
+        this.users = this.users.filter((user) => user !== userId);
+      }
+
+      isEmpty() {
+        return this.users.length === 0;
       }
     }
-    // generate a unique ID for each user
+
     const generateUniqueId = () => {
       const timestamp = Date.now().toString(36);
       const randomStr = Math.random().toString(36).substring(2, 8);
       return `${timestamp}-${randomStr}`;
     };
 
-    let queuedUsers = [];
     let privateRooms = [];
-    let users = {};
+    let users = {}; // Maps Google User ID to Socket ID
 
     await app.prepare();
     const server = express();
@@ -46,64 +52,49 @@ const startServer = async () => {
       },
     });
 
-    // Socket.IO events
     io.on("connection", (socket) => {
       socket.on("authenticate", (googleUserId) => {
         console.log(`Authenticating user with Google ID: ${googleUserId}`);
-        queuedUsers.push(users[googleUserId]);
-        // If the user already has a socketId, remove the old one
-        if (users[googleUserId]) {
-          console.log(`Removing old socketId for user: ${googleUserId}`);
-          io.sockets.sockets.get(users[googleUserId])?.disconnect();
-          queuedUsers = queuedUsers.filter((id) => id !== users[googleUserId]);
 
-          // remove private room if user is in one
+        if (users[googleUserId]) {
+          const oldSocketId = users[googleUserId];
+          io.sockets.sockets.get(oldSocketId)?.disconnect();
+          privateRooms = privateRooms.filter((room) => {
+            room.removeUser(oldSocketId);
+            return !room.isEmpty();
+          });
         }
 
-        // Save the new socketId for this user
         users[googleUserId] = socket.id;
-        console.log(users);
+        console.log(
+          `User ${googleUserId} is now authenticated with socket ${socket.id}`
+        );
       });
 
-      console.log(`${socket.id} has connected`);
-
-      //check if user has a session
-
-      console.log(queuedUsers);
-
-      //joining a private chat room
       socket.on("joinPrivateChat", (googleUserId) => {
-        const createPrivateRoom = () => {
-          const privateRoom = new room(generateUniqueId());
-          const key = users[googleUserId];
-          console.log(`your key is ${key}`);
-          privateRoom.addItem(key);
-          privateRooms.push(privateRoom);
-          socket.join(privateRoom.id);
-          console.log(
-            `${socket.id} has created private chat$ room and joined that room too ${privateRoom.id}`
-          );
-        };
-
-        const availableRoom = privateRooms.find(
-          (room) =>
-            room.users.length === 1 && room.users[0] != users[googleUserId]
+        let availableRoom = privateRooms.find(
+          (room) => room.users.length === 1
         );
 
-        if (availableRoom) {
-          availableRoom.addItem(users[googleUserId]);
+        if (availableRoom && availableRoom.addUser(socket.id)) {
           socket.join(availableRoom.id);
-          io.to(availableRoom.id).emit("matchFound");
+          io.to(availableRoom.id).emit("matchFound", {
+            roomId: availableRoom.id,
+          });
           console.log(
-            `${users[googleUserId]} joined existing private room: ${availableRoom.id}`
+            `${googleUserId} joined existing private room: ${availableRoom.id}`
           );
         } else {
-          // Create a new private room
-          createPrivateRoom();
+          const newRoom = new Room(generateUniqueId());
+          newRoom.addUser(socket.id);
+          privateRooms.push(newRoom);
+          socket.join(newRoom.id);
+          console.log(
+            `${googleUserId} created and joined new private room: ${newRoom.id}`
+          );
         }
       });
 
-      //sending a message to a private chat room
       socket.on("sendPrivateMessage", (data) => {
         const userRoom = privateRooms.find((room) =>
           room.users.includes(socket.id)
@@ -111,47 +102,24 @@ const startServer = async () => {
 
         if (userRoom) {
           console.log(
-            `${socket.id} sent a message to room ${userRoom.id}: ${data.actualMessage}`
+            `Message sent in room ${userRoom.id}: ${data.actualMessage}`
           );
+
+          // Emit message to all users in the room *except* the sender
           socket.to(userRoom.id).emit("receivePrivateMessage", data);
-        } else {
-          console.error(`User ${socket.id} is not in any room.`);
         }
       });
 
       socket.on("disconnect", () => {
-        console.log(`client ${socket.id} disconnected`);
-        queuedUsers = queuedUsers.filter((item) => item != socket.id);
-
+        console.log(`Client ${socket.id} disconnected`);
         privateRooms = privateRooms.filter((room) => {
-          const userIndex = room.users.indexOf(socket.id);
-
-          if (userIndex !== -1) {
-            // If the user is in this room, remove them
-            room.users.splice(userIndex, 1);
-
-            // If the room has no users left, remove the room from privateRooms
-            if (room.users.length === 0) {
-              console.log(`Room ${room.id} is empty and will be removed`);
-              return false; // Remove the room
-            }
-          }
-
-          // Keep the room if the user is not in it
-          return true;
+          room.removeUser(socket.id);
+          return !room.isEmpty();
         });
       });
     });
 
-    // Your custom routes here
-    server.get("/api/custom", (req, res) => {
-      res.json({ message: "Custom route" });
-    });
-
-    // Handle all other routes with Next.js
-    server.all("*", (req, res) => {
-      return handle(req, res);
-    });
+    server.all("*", (req, res) => handle(req, res));
 
     httpServer.listen(port, () => {
       console.log(`> Ready on http://localhost:${port}`);
