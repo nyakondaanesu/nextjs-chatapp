3fe828e05c124072559149bf38de3f36876fdcf3
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-11 11:29:55 +0200

    adding missing space page

---
+++ app/space/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className="flex flex-col h-screen justify-center items-center">
+        <h1>coming soon</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
