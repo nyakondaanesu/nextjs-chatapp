e1237293446c91008644037f9c00ad2b86adeb17
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-13 14:46:08 +0200

    revert to normal layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-main flex h-screen  `}
       >
-        <Providers>{children}</Providers>
+        {children}
       </body>
     </html>
   );
