863882f61371ca7725f7a7f221f47c0741ac5fac
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-16 12:55:40 +0200

    new hero image better spacing

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,18 @@ export default function Home() {
       </header>
 
       <section className="flex flex-col md:flex-row items-center justify-center flex-grow px-6 md:px-12">
-        <div className="md:w-1/2 space-y-4 text-center md:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold text-black">
+        <div className="md:w-[40%] space-y-4 text-center md:text-left">
+          <h1 className="text-5xl md:text-7xl font-bold text-black">
             Chat with strangers without a Hitch
           </h1>
-          <h3 className="text-sm font-light text-black">
+          <h3 className="text-sm font-light text-black mt-5">
             Meet new people, start conversations, and make connections—all in
             real-time.
           </h3>
 
-          <form action={handleSignIn} className="mt-10">
+          <form action={handleSignIn} className="">
             <button
-              className="flex items-center gap-3 border-2 border-zinc-800 text-black rounded-lg px-6 py-2 hover:bg-gray-100 transition"
+              className="flex items-center gap-3 border-2 border-zinc-800 text-black rounded-lg px-6 py-2 hover:bg-gray-100 transition mt-10"
               type="submit"
             >
               <Image src="/google.png" alt="Google" width={20} height={20} />
@@ -34,7 +34,7 @@ export default function Home() {
           </form>
         </div>
 
-        <div className="md:w-1/2 mt-10 md:mt-0 md:ml-10">
+        <div className="md:w-[60%] mt-20 md:mt-0 md:ml-10">
           <Image
             src="/heroImageIllustration.png"
             alt="hero"
@@ -43,23 +43,23 @@ export default function Home() {
           />
         </div>
       </section>
-      <footer className="mx-2 bg-whit mt-10 mb-2">
-        <hr className="md:mx-25 mt-10 border-zinc-800 " />
+      <footer className="mx-2  mt-10 mb-2">
+        <hr className="md:mx-25 mt-10 border-zinc-300 " />
         <div className="flex justify-between mt-10">
-          <p className="font-light text-xs md:mx-28 tex-zinc-300 text-dark">
+          <p className="font-light text-xs md:mx-28  text-black">
             &copy;{new Date().getFullYear()} Anesu Nyakonda
           </p>
           <div className="flex space-x-5">
             <Link
               href={"/terms"}
-              className="font-light text-xs underline pr-10 text-dark"
+              className="font-light text-xs underline pr-10 text-black"
             >
               Terms and conditions
             </Link>
 
             <Link
               href={"/terms"}
-              className="font-light text-xs underline pr-10 text-dark"
+              className="font-light text-xs underline pr-10 text-black"
             >
               {" "}
               Privacy Policy
